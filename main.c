1633f15764b10799cc22a66a866eef330a526c6a
Allan Barbot <75270400+CurtainShow@users.noreply.github.com>
2021-10-21 00:06:55 +0200

    Create main.c
    Création de map avec choix de taille et mur + début de remplissage random

---
+++ main.c
@@ -0,0 +1,62 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void droite(int **mat) {
+
+}
+
+int main() {
+    int **mat, row, col, i, j, n;
+
+    printf("Welcome to Malloc World !");
+    printf("\n");
+
+    //printf("How many rows you want to create?");
+    printf("Choose a size for your game grid :");
+    printf("\n");
+    scanf("%d", &row);
+    mat = (int **)malloc(row*sizeof(int *));
+
+    //printf("How many column you want to create?");
+    //scanf("%d", &col);
+
+    col = row;
+
+    for(i=0;i<row;i++) {
+        mat[i]=(int *)malloc(col*sizeof(int));
+    }
+
+    for(i=0;i<row;i++) {
+        for(j=0;j<col;j++) {
+            if (i == 0)
+            {
+                mat[i][j] = 1;
+            }
+            else if (i == row-1){
+                mat[i][j] = 1;
+            }
+            else if (j == 0){
+                mat[i][j] = 1;
+            }
+            else if (j == col-1){
+                mat[i][j] = 1;
+            }
+            else if (j == 1 && i == 1){
+                mat[i][j] = 3;
+            }
+            else {
+                n = rand()%10 + 4;
+                mat[i][j] = n;
+            }
+        }
+    }
+
+    for(i=0;i<row;i++) {
+        for(j=0;j<row;j++) {
+            printf("%d\t", mat[i][j]);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
